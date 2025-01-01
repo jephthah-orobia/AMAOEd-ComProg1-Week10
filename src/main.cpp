3185869d0fae840bb79eec24c7da8d8e4c3a32e9
jephthah-orobia <jephthah.orobia@gmail.com>
2025-01-01 00:04:52 -0600

    update readme and app description in comments of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /* ****************************************************************
- * @author: <your name here>
- * @app name: <your application name here>
+ * @author: Jephthah M. Oroba
+ * @app name: AMOEd-ComProg1-Week10 LabExercise009
  * @app desc: <describe the purpose of your application here>
  * @history:
  *  - <yyyy/mm/dd> | <name>
@@ -20,25 +20,9 @@
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-//                               NOTE
-// This is your program entry point. Your main logic is placed
-// inside this function. You may add your functions before this
-// "main()", or after this "main()" provided you added reference
-// before this "main()" function.
-//////////////////////////////////////////////////////////////////
-
 int main()
 {
-  // ************************** TO DO **************************
-  // Place your code logic after this comment line
-  // ***********************************************************
-
   cout << "Hello World!" << endl;
-
-  // ********************** DO NOT CHANGE **********************
-  // Print a new line and ask user for any key before exiting
-  // ***********************************************************
   _pause();
   return EXIT_SUCCESS;
 }
